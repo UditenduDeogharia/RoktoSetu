9be297560fc0503a32583d74ac0f4bd3bc1ff758
UditenduDeogharia <uditendudeogharia2006@gmail.com>
2026-09-25 04:48:59 +0530

    Enable Supabase authentication gate across RoktoSetu

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
+import AuthGate from '../components/AuthGate';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <AuthGate><Component {...pageProps} /></AuthGate>;
 }
