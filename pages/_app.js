7eddfb7d9f84c6b218175fd6067f6c8223c146d9
UditenduDeogharia <uditendudeogharia2006@gmail.com>
2026-09-25 05:44:50 +0530

    Integrate Phase 6-7 feature hub

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import AuthGate from '../components/AuthGate';
+import Phase67Hub from '../components/Phase67Hub';
 
 export default function App({ Component, pageProps }) {
-  return <AuthGate><Component {...pageProps} /></AuthGate>;
+  return <AuthGate><Component {...pageProps}/><Phase67Hub/></AuthGate>;
 }
